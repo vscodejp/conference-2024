1bb81c721bc03070ca7b52ad9ad349a70b3fdf09
cssho <rizeupbass@gmail.com>
2024-03-24 00:02:04 +0900

    purge css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vscodejp.github.io",
   base: "/conference-2024",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), purgecss()]
 });
