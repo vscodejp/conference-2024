759a2d8b4d881e7a3fb2b6bc1619c51ba7111b40
cssho <rizeupbass@gmail.com>
2024-03-24 00:55:17 +0900

    Revert "purge css"
    This reverts commit 1bb81c721bc03070ca7b52ad9ad349a70b3fdf09.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import purgecss from "astro-purgecss";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://vscodejp.github.io",
   base: "/conference-2024",
-  integrations: [tailwind(), icon(), purgecss()]
+  integrations: [tailwind(), icon()],
 });
